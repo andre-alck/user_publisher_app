1dd861f3a5435e63ded5757c99db6b9a52db8c14
andre-alck <andrealck1@gmail.com>
2022-10-26 15:28:26 -0300

    feat: add services for amqp control

--- lib/service/amqp_controller_service.dart
+++ lib/service/amqp_controller_service.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:dart_amqp/dart_amqp.dart';
-import 'package:p_user_publisher/models/user.dart';
 
 class AMQPControllerService {
   Future<void> send(
-    User user,
+    String username,
   ) async {
     ConnectionSettings connectionSettings = ConnectionSettings(
       maxConnectionAttempts: 5,
@@ -22,9 +24,76 @@ class AMQPControllerService {
 
     exchange.publish(
       'OK',
-      user.name,
+      username,
     );
 
     client.close();
+  }
+
+  Future<String> receive(
+    String username,
+  ) async {
+    final connectionSettings = ConnectionSettings(
+      host: '10.0.2.2',
+      port: 5672,
+    );
+    Client client = Client(
+      settings: connectionSettings,
+    );
+
+    ProcessSignal.sigint.watch().listen(
+      (
+        event,
+      ) async {
+        await client.close();
+        exit(
+          0,
+        );
+      },
+    );
+
+    List<String> routingKeys = [];
+    routingKeys.add(
+      username,
+    );
+
+    final Completer<String> completer = Completer<String>();
+
+    client.channel().then(
+      (
+        Channel channel,
+      ) {
+        return channel.exchange(
+          "logs",
+          ExchangeType.DIRECT,
+          durable: false,
+        );
+      },
+    ).then(
+      (
+        Exchange exchange,
+      ) {
+        return exchange.bindPrivateQueueConsumer(
+          routingKeys,
+          consumerTag: "logs",
+          noAck: true,
+        );
+      },
+    ).then(
+      (
+        Consumer consumer,
+      ) {
+        consumer.listen(
+          (
+            AmqpMessage message,
+          ) =>
+              completer.complete(
+            message.payloadAsString,
+          ),
+        );
+      },
+    );
+
+    return completer.future;
   }
 }
